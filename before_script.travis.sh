1f3984ea5a38859872c05767223c0fba013bfb55
klezm <20573323+klezm@users.noreply.github.com>
2017-12-03 14:57:57 +0100

    ...

--- before_script.travis.sh
+++ before_script.travis.sh
@@ -5,7 +5,6 @@ repoName="https://github.com/$1"
 echo "sed before script >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ls -lha
 
-github
 ribbonCode="<a href='$repoName' style='position:fixed;padding:5px 45px;width:128px;bottom:50px;left:-50px;\
 -webkit-transform:rotate(45deg);-moz-transform:rotate(45deg);-ms-transform:rotate(45deg);transform:rotate(45deg);\
 box-shadow:0 0 0 3px #182c42, 0 0 20px -3px rgba(0, 0, 0, 0.5);text-shadow:0 0 0 #ffffff, 0 0 5px rgba(0, 0, 0, 0.3);\
