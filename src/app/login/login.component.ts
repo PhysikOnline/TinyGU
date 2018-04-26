7cd39deb4a3266d6e4576bfc9342884bd508a495
Bomel <keiwanjamaly@hotmail.de>
2018-04-27 00:13:24 +0200

    added the first visuals for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('');
 
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  getEmailErrorMessage() {
+    return this.email.hasError('required') ? 'This can not be empty' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+
+  getPasswordErrorMessage() {
+    let errorMessage: string;
+    if (this.password.hasError('required') && this.email.hasError('required')) {
+      errorMessage = 'This too xD';
+    } else if (this.password.hasError('required')) {
+      errorMessage = 'This can not be empty';
+    } else {
+      errorMessage = '';
+    }
+
+    return errorMessage;
+  }
 }
